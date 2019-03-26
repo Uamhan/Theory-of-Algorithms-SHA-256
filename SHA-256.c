16707d80f5048771f955159018bed8f3291ab07c
uamhan <uamhanl@gmail.com>
2019-03-26 17:42:17 +0000

    added working variables to sha256 method

--- SHA-256.c
+++ SHA-256.c
@@ -22,4 +22,25 @@ int main(int argc, char *argv[]){
 //sha256 method implementation
 void sha256(){
 
+    //Message schedule (section 6.2)
+    uint32_t w[64];
+    //Working Variable (section 6.2)
+    uint32_t a,b,c,d,e,f,g,h;
+    //Two temporary variables (section 6.2)s
+    uint32_t T1,T2;
+
+    //The hash Value (section 6.2)
+    //Values come from Section 5.3.3
+    uint32_t H[8] = {
+        0x6a09e667,
+        0xbb67ae85,
+        0x3c6ef372,
+        0xa54ff53a,
+        0x510e527f,
+        0x9b05688c,
+        0x1f83d9ab,
+        0x5be0cd19
+    };
+
+
 }
