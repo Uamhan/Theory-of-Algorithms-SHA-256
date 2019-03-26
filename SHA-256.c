16ae3c1de6b0b2713956f779f00e529b88252475
uamhan <uamhanl@gmail.com>
2019-03-26 17:24:58 +0000

    added initial comments and header files

---
+++ SHA-256.c
@@ -0,0 +1,11 @@
+//Uamhan Mac Fheaghusa
+//Secure Hash Algorithm, 256 bit version
+
+//link to Secure Hash Algorithm Standard
+//https://www.nist.gov/publications/secure-hash-standard
+
+//standard input/output header file
+#include <stdio.h>
+//fixed bit integer header file
+#include <stdint.h>
+
