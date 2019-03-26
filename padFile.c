b2022bd35e7416cee1d518c77884d9d5b0f23062
uamhan <uamhanl@gmail.com>
2019-03-26 23:33:09 +0000

    added padding functionality for PAD1 and PAD0

--- padFile.c
+++ padFile.c
@@ -5,7 +5,7 @@ union msgblock {
     uint8_t e[64];
     uint32_t t[16];
     uint64_t s[8];
-}
+};
 
 enum status {READ,PAD0,PAD1,FINISH};
 
@@ -19,6 +19,8 @@ int main(int argc, char *argv[]){
     enum status S = READ;
 
     union msgblock M;
+    //iterator for forloop
+    int i;
 
     //TODO implement error checking
     f = fopen(argv[1],"r");
@@ -42,14 +44,24 @@ int main(int argc, char *argv[]){
                 nobytes = nobytes + 1;
                 M.e[nobytes] = 0x00;
             }
-        }else{
-
+        }else if(feof(f)){
+            S=PAD1;
         }
-
-
-        
     }
+
+    if(S==PAD0 || S==PAD1){
+        for(i = 0; i < 56; i++)
+            M.e[i] = 0x00;
+        M.s[7] = nobits;
+    }
+    if(S==PAD1)
+        M.e[0] = 0x80;
+
     fclose(f);
+
+    for(i=0;i<64;i++)
+        printf("%x",M.e[i]);
+    printf("\n");
     
     return 0;
 }
