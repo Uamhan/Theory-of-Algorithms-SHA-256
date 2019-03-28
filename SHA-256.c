f45a4a3bdf7d06314598ef896da66b8f7d90f093
uamhan <uamhanl@gmail.com>
2019-03-28 23:45:43 +0000

    hashing algorithm producing incorrect results. added buffer state structure as start of attempt to rectafy problem

--- SHA-256.c
+++ SHA-256.c
@@ -9,6 +9,15 @@
 //fixed bit integer header file
 #include <stdint.h>
 
+// buffer state struct that holds information regarding the message to be hashed during padding
+struct buffer_state {
+	const uint8_t * p;
+	size_t len;
+	size_t total_len;
+	int single_one_delivered; /* bool */
+	int total_len_delivered; /* bool */
+};
+
 //union structure message block
 union msgblock {
     uint8_t e[64];
@@ -42,6 +51,8 @@ uint32_t Maj(uint32_t x,uint32_t y,uint32_t z);
 uint32_t rotr(uint32_t n, uint32_t X);
 uint32_t shr(uint32_t n, uint32_t X);
 
+
+
 int main(int argc, char *argv[]){
     //user input file
     FILE* msgf;
@@ -53,6 +64,8 @@ int main(int argc, char *argv[]){
     sha256(msgf);
     //close file
     fclose(msgf);
+    
+
     return 0;
 }
 
@@ -73,11 +86,12 @@ int nextmsgblock(FILE *msgf, union msgblock *M, enum status *S, uint64_t *nobits
         //set last 64 bits to the number of bits in the file (should be big endian)
         M->s[7] = *nobits;
         //set s to finish
-        *S = FINISH;
+        
         //if s was pad1 set the first bit of m to one.
         if(*S==PAD1)
             M->e[0] = 0x80;
         // keep the loop in sha256 going for one more iteration
+        *S = FINISH;
         return 1;
     }
     
@@ -132,19 +146,18 @@ void sha256(FILE *msgf){
 
     // K Constants. defined section 4.2.2.
     uint32_t K[] = {
-        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
 
 
-    //Message schedule (section 6.2)
-    uint32_t W[64];
+    
     //Working Variable (section 6.2)
     uint32_t a,b,c,d,e,f,g,h;
     //Two temporary variables (section 6.2)s
@@ -167,13 +180,17 @@ void sha256(FILE *msgf){
     int i,t;
 
     while(nextmsgblock(msgf,&M,&S,&nobits)){
+
+        //Message schedule (section 6.2)
+        uint32_t W[64];
+
         // from page 22 of standard, W[t] = M[t] for 0 <= t <= 15.
         for (t = 0; t < 16; t++)
-            W[t] = M.t[t];
+            W[t] = LToBEndian(M.t[t]);
     
         // from page 22, W[t] = ...
         for (t=16; t<64; t++)
-        W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+            W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
         
         // Initialise a,b,c,d,e,f,g and h as per step 2 page 19
         a = H[0];
@@ -211,16 +228,12 @@ void sha256(FILE *msgf){
         
     }
     //coverts from little to big endian
-    H[0]=LToBEndian(H[0]);
-    H[1]=LToBEndian(H[1]);
-    H[2]=LToBEndian(H[2]);
-    H[3]=LToBEndian(H[3]);
-    H[4]=LToBEndian(H[4]);
-    H[5]=LToBEndian(H[5]);
-    H[6]=LToBEndian(H[6]);
-    H[7]=LToBEndian(H[7]);
+    
     //prints results to console
     printf("%08x %08x %08x %08x %08x %08x %08x %08x \n",H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
+    //for (i = 0; i <32; i++ ){
+    //    printf("%x ",hash[i]);
+    //}
 }
 //method converts from little endian to big endian 
 uint32_t LToBEndian(uint32_t x)
@@ -260,3 +273,5 @@ uint32_t Ch(uint32_t x,uint32_t y,uint32_t z){
 uint32_t Maj(uint32_t x,uint32_t y,uint32_t z){
     return ((x & y) ^ (x & z) ^ (y & z));
 }
+
+
