aeefcadfeda1d31b265ec364788f12a8613ab1c9
uamhan <uamhanl@gmail.com>
2019-03-29 02:09:26 +0000

    failed to debug the hashing section of algorithm so reimplemented it following the standards document.

--- SHA-256.c
+++ SHA-256.c
@@ -41,7 +41,7 @@ void sha256(uint8_t hash[32], char input[], size_t len);
 //little to big endianan method definition
 unsigned int LToBEndian(uint32_t x);
 //added method to initilse the bufferstate structure
-static void initBufferState(struct buffer_state * state, const void * input, size_t len);
+static void initBufferState(struct buffer_state * state, char input[], size_t len);
 
 //next message block method defination handles the padding of each message block.
 int nextmsgblock(uint8_t block[BLOCK_SIZE], struct buffer_state * state);
@@ -58,23 +58,32 @@ uint32_t Ch(uint32_t x,uint32_t y,uint32_t z);
 uint32_t Maj(uint32_t x,uint32_t y,uint32_t z);
 
 //see Section 3.2 for definitions
-uint32_t rotr(uint32_t n, uint32_t X);
+static inline uint32_t rotr(uint32_t value, unsigned int count);
 uint32_t shr(uint32_t n, uint32_t X);
 
-
+// K Constants. defined section 4.2.2.
+static const uint32_t k[] = {
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
 
 int main(int argc, char *argv[]){
-    //user input file
-    FILE* msgf;
 
-    //TODO implement error checking
-    //openfile given as first commandline argument
-    msgf = fopen(argv[1],"r");
-    //executes the sha256 algorithm on the file
-    sha256(msgf);
-    //close file
-    fclose(msgf);
-    
+    //aray that will hold the hashed message
+    uint8_t hash[32];
+    //input to be hashed
+    char input[] = "abc";
+    //hash algorithm that fills the hash array with the hashed message
+    sha256(hash, input, strlen(input));
+    //prints the hashed message
+    for(int i = 0; i <32;i++)
+        printf("%x",hash[i]);
 
     return 0;
 }
@@ -125,7 +134,7 @@ int nextmsgblock(uint8_t block[BLOCK_SIZE], struct buffer_state * state){
     //or there isint enough space left and we need to padd the rest of this block with zeros
     //in this case there will be enough space in this block to finish next time this method is run.
     //if Space in block is greater than the the total length left in the message
-	if  spaceInBlock >= TOTAL_LEN_LEN) {
+	if(spaceInBlock >= TOTAL_LEN_LEN) {
         //take total length left away from spaceInBlock
 		const size_t left = spaceInBlock - TOTAL_LEN_LEN;
         //len is set to the total length left in the block
@@ -148,7 +157,7 @@ int nextmsgblock(uint8_t block[BLOCK_SIZE], struct buffer_state * state){
 		}
         //sets the total length delivered varible in state to done
 		state->total_len_delivered = 1;
-	} else {
+	}else {
         //otherwise fill the current block with 0's
 		memset(block, 0x00, spaceInBlock);
 	}
@@ -160,103 +169,85 @@ int nextmsgblock(uint8_t block[BLOCK_SIZE], struct buffer_state * state){
 //sha256 method implementation
 void sha256(uint8_t hash[32], char input[], size_t len){
 
-    uint64_t nobits = 0;
-
-    //current status of the file
-    enum status S = READ;
-    //the message block
-    union msgblock M;
-
-    // K Constants. defined section 4.2.2.
-    uint32_t K[] = {
-	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-    };
-
-
-    
-    //Working Variable (section 6.2)
-    uint32_t a,b,c,d,e,f,g,h;
-    //Two temporary variables (section 6.2)s
-    uint32_t T1,T2;
-
-    //The hash Value (section 6.2)
+    //The initial hash Values (section 6.2)
     //Values come from Section 5.3.3
-    uint32_t H[8] = {
+    uint32_t h[] = { 
         0x6a09e667,
-        0xbb67ae85,
-        0x3c6ef372,
-        0xa54ff53a,
-        0x510e527f,
-        0x9b05688c,
-        0x1f83d9ab,
-        0x5be0cd19
-    };
-    
-    //forloop interator
-    int i,t;
+        0xbb67ae85, 
+        0x3c6ef372, 
+        0xa54ff53a, 
+        0x510e527f, 
+        0x9b05688c, 
+        0x1f83d9ab, 
+        0x5be0cd19 };
+	
+    int i, j;
 
-    while(nextmsgblock(msgf,&M,&S,&nobits)){
+	// block that will be hashed by the algorithm
+	uint8_t block[64];
+    //state struct that holds the state of the message to be hashed
+	struct buffer_state state;
+    //initilises the state struct.
+	initBufferState(&state, input, len);
 
-        //Message schedule (section 6.2)
-        uint32_t W[64];
+    //loop that iterates through the message to be hashed block by block calling nextmsgblock each iteration to padd the block.
+	while (nextmsgblock(block, &state)) {
+		uint32_t workingSet[8];
+		
+        //array that holds the message that will be hashed by the algorithm
+		uint32_t w[64];
+        //current padded block for the iteration
+		const uint8_t *p = block;
+        //fills the w array with 0's to initilise it before we use it.
+		memset(w, 0x00, sizeof w);
+        //from page 22 of standard, W[t] = M[t] for 0 <= t <= 15 with big endian coversion.
+		for (i = 0; i < 16; i++) {
+			w[i] = (uint32_t) p[0] << 24 | (uint32_t) p[1] << 16 | (uint32_t) p[2] << 8 | (uint32_t) p[3];
+			p += 4;
+		}
 
-        // from page 22 of standard, W[t] = M[t] for 0 <= t <= 15.
-        for (t = 0; t < 16; t++)
-            W[t] = LToBEndian(M.t[t]);
-    
-        // from page 22, W[t] = ...
-        for (t=16; t<64; t++)
-            W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
-        
-        // Initialise a,b,c,d,e,f,g and h as per step 2 page 19
-        a = H[0];
-        b = H[1];
-        c = H[2];
-        d = H[3];
-        e = H[4];
-        f = H[5];
-        g = H[6];
-        h = H[7];
+		// from page 22, w[t] = ...
+		for (i = 16; i < 64; i++) {
+			const uint32_t s0 = rotr(w[i - 15], 7) ^ rotr(w[i - 15], 18) ^ (w[i - 15] >> 3);
+			const uint32_t s1 = rotr(w[i - 2], 17) ^ rotr(w[i - 2], 19) ^ (w[i - 2] >> 10);
+			w[i] = w[i - 16] + s0 + w[i - 7] + s1;
+		}
+		
+		//initilise working set a-h as per step 2 page 19 for this implemtation they will be stored in the array workingSet. workingSet[0] == a workingset[1] == b ect...
+		for (i = 0; i < 8; i++)
+			workingSet[i] = h[i];
 
-        //step 3 creating new temp values for working varibales defined above.
-        for (t = 0; t<64; t++){
-            T1 = h + SIG1(e) + Ch(e,f,g) + K[t] + W[t];
-            T2 = SIG0(a) + Maj(a,b,c);
-            h = g;
-            g = f;
-            f = e;
-            e = d+T1;
-            d = c;
-            c = b;
-            b = a;
-            a = T1+T2;
-        }
+		//step 3 creating new temp values for working varibales defined above.
+		for (i = 0; i < 64; i++) {
+			const uint32_t s1 = rotr(workingSet[4], 6) ^ rotr(workingSet[4], 11) ^ rotr(workingSet[4], 25);
+			const uint32_t ch = (workingSet[4] & workingSet[5]) ^ (~workingSet[4] & workingSet[6]);
+			const uint32_t temp1 = workingSet[7] + s1 + ch + k[i] + w[i];
+			const uint32_t s0 = rotr(workingSet[0], 2) ^ rotr(workingSet[0], 13) ^ rotr(workingSet[0], 22);
+			const uint32_t maj = (workingSet[0] & workingSet[1]) ^ (workingSet[0] & workingSet[2]) ^ (workingSet[1] & workingSet[2]);
+			const uint32_t temp2 = s0 + maj;
 
-        //step 4
-        H[0] = a + H[0];
-        H[1] = b + H[1];
-        H[2] = c + H[2];
-        H[3] = d + H[3];
-        H[4] = e + H[4];
-        H[5] = f + H[5];
-        H[6] = g + H[6];
-        H[7] = h + H[7];
-        
-    }
-    //coverts from little to big endian
-    
-    //prints results to console
-    printf("%08x %08x %08x %08x %08x %08x %08x %08x \n",H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
-    //for (i = 0; i <32; i++ ){
-    //    printf("%x ",hash[i]);
-    //}
+			workingSet[7] = workingSet[6];
+			workingSet[6] = workingSet[5];
+			workingSet[5] = workingSet[4];
+			workingSet[4] = workingSet[3] + temp1;
+			workingSet[3] = workingSet[2];
+			workingSet[2] = workingSet[1];
+			workingSet[1] = workingSet[0];
+			workingSet[0] = temp1 + temp2;
+		}
+        // add the new hashed block to the current hash values
+		for (i = 0; i < 8; i++)
+			h[i] += workingSet[i];
+	}
+
+	//adds fiinished hash set to the hash array as big endian.
+	for (i = 0, j = 0; i < 8; i++)
+	{
+		hash[j++] = (uint8_t) (h[i] >> 24);
+		hash[j++] = (uint8_t) (h[i] >> 16);
+		hash[j++] = (uint8_t) (h[i] >> 8);
+		hash[j++] = (uint8_t) h[i];
+	}
 }
 
 static void initBufferState(struct buffer_state * state, char input[], size_t len){
@@ -275,8 +266,8 @@ uint32_t LToBEndian(uint32_t x)
 }
 
 //see section 3.2 for definitions
-uint32_t rotr(uint32_t n, uint32_t X){
-    return (X >> n) | (X << (32-n));
+static inline uint32_t rotr(uint32_t value, unsigned int count){
+	return value >> count | value << (32 - count);
 }
 //see section 3.2 for definitions
 uint32_t shr(uint32_t n, uint32_t X){
